19e8a4eca9703ea7e81d1dd887cfe30c07e1a1bd
leen-23 <leen.harbi87@gmail.com>
2024-05-08 20:44:53 +0300

    my_streamlit_app.py

--- .devcontainer/my_streamlit_app.py
+++ .devcontainer/my_streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import SVC
 import joblib
+pip install scikit-learn
 
 # تحميل النموذج المدرب والمتجه
 model = joblib.load("model.pkl")
